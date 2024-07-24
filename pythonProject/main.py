b8e5a6f054b43a3e402a42b5b39db4e95ccea488
Leticia <ltcnb@hotmail.com>
2024-07-23 21:47:07 -0300

    initial commit

---
+++ pythonProject/main.py
@@ -0,0 +1,14 @@
+import pygame
+
+print('Setup start')
+pygame.init()
+window = pygame.display.set_mode(size=(600, 480)) # criando a janela que rodará o jogo
+print('Setup End')
+
+print('loop start')
+while True:
+    # check for all events (analise para todos eventos)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: #adicionando o evento para tirar erro de fechar a janela
+            pygame.quit() # comando para fechar janela
+            quit() # encerra o pygame
